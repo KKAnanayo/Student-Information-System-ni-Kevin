ee0798285e63f4c6e2f763c9ce4115cea051620d
kevin <jadenyuki486@gmail.com>
2024-04-02 07:30:36 +0800

    minor change

--- dashboard/src/Pages/ViewUsers.js
+++ dashboard/src/Pages/ViewUsers.js
@@ -19,10 +19,10 @@ function ViewUsers() {
 
     const [firstNameError, setFirstNameError] = useState(false);
     const [lastNameError, setLastNameError] = useState(false);
-    const [middleNameError, setMiddleNameError] = useState(false);
     const [emailRequiredError, setEmailRequiredError] = useState(false);
     const [emailUniqueError, setEmailUniqueError] = useState(false);
     const [passwordError, setPasswordError] = useState(false);
+
 
     useEffect(() => {
         fetchData();
@@ -60,7 +60,6 @@ function ViewUsers() {
         setEmailRequiredError(false);
         setFirstNameError(false);
         setLastNameError(false);
-        setMiddleNameError(false);
         setPasswordError(false);
         setEditFirst("");
         setEditLast("");
@@ -70,10 +69,13 @@ function ViewUsers() {
         
     };
 
+  
+
     function handleAddUser () {
         
         if (!editFirst) {
             setFirstNameError(true);
+            return;
         } else {
             setFirstNameError(false);
         }
@@ -81,14 +83,9 @@ function ViewUsers() {
          
         if (!editLast) {
             setLastNameError(true);
+            return;
         } else {
             setLastNameError(false);
-        }
-
-        if (!editMiddle) {
-            setMiddleNameError(true);
-        } else {
-            setMiddleNameError(false);
         }
 
         if (!editEmail) {
@@ -105,6 +102,7 @@ function ViewUsers() {
             setPasswordError(false);
         }
 
+        
         const userData = {
             First: editFirst,
             Last: editLast,
@@ -115,7 +113,6 @@ function ViewUsers() {
 
         axios.post("http://localhost:1337/addUser", userData)
         .then(response => {
-          
     
             console.log("User added successfully:", response.data);
             setEditFirst("");
@@ -135,6 +132,34 @@ function ViewUsers() {
     }
 
     function handleSaveEdit () {
+        if (!editFirst) {
+            setFirstNameError(true);
+            return;
+        } else {
+            setFirstNameError(false);
+        }
+
+        if (!editLast) {
+            setLastNameError(true);
+            return;
+        } else {
+            setLastNameError(false);
+        }
+
+        if (!editEmail) {
+            setEmailRequiredError(true);
+            return;
+        } else {
+            setEmailRequiredError(false);
+        }
+
+        if (!editPassword) {
+            setPasswordError(true);
+            return;
+        } else {
+            setPasswordError(false);
+        }
+        
         const userData = {
             First: editFirst,
             Last: editLast,
@@ -163,18 +188,7 @@ function ViewUsers() {
                 <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', boxShadow: 24, p: 4 }}>
                 <Typography variant="h6" component="h2" fontWeight="bold" align="left">Add Userr Information</Typography>
                     <div style={{ marginBottom: '16px' }} />
-                    <TextField variant="outlined" 
-                    label="Email" 
-                    value={editEmail} 
-                    onChange={(e) => setEditEmail(e.target.value)} 
-                    error={emailRequiredError || emailUniqueError}
-                    helperText={
-                        (emailRequiredError && "Email is required") ||
-                        (emailUniqueError && "Email must be unique")
-                    }
-                    />
-                    <div style={{ marginBottom: '16px' }} 
-                    />
+                    
                     <TextField variant="outlined" 
                     label="First Name" 
                     value={editFirst} onChange={(e) => 
@@ -195,10 +209,21 @@ function ViewUsers() {
                     label="Middle Name" 
                     value={editMiddle} 
                     onChange={(e) => setEditMiddle(e.target.value)} 
-                    error={middleNameError}
-                    helperText={middleNameError && "Middle Name is required"}
                     />
+    
                     <div style={{ marginBottom: '16px' }} />
+                    <TextField variant="outlined" 
+                    label="Email" 
+                    value={editEmail} 
+                    onChange={(e) => setEditEmail(e.target.value)} 
+                    error={emailRequiredError || emailUniqueError}
+                    helperText={
+                        (emailRequiredError && "Email is required") ||
+                        (emailUniqueError && "Email must be unique")
+                    }
+                    />
+                    <div style={{ marginBottom: '16px' }} 
+                    />
                     <TextField type="password" 
                     variant="outlined" 
                     label="Password" 
@@ -217,20 +242,20 @@ function ViewUsers() {
                 <Table sx={{ minWidth: 100 }} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell align="center">Email</TableCell>
-                            <TableCell align="center">First Name</TableCell>
-                            <TableCell align="center">Last Name</TableCell>
-                            <TableCell align="center">Middle Name</TableCell>
-                            <TableCell align="center">Actions</TableCell>
+                            <TableCell align="center"><b>First Name</b></TableCell>
+                            <TableCell align="center"><b>Last Name</b></TableCell>
+                            <TableCell align="center"><b>Middle Name</b></TableCell>
+                            <TableCell align="center"><b>Email</b></TableCell>
+                            <TableCell align="center"><b>Actions</b></TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {users && users.map(user => (
                             <TableRow key={user.Email}>
-                                <TableCell align="center">{user.Email}</TableCell>
                                 <TableCell align="center">{user.First}</TableCell>
                                 <TableCell align="center">{user.Last}</TableCell>
                                 <TableCell align="center">{user.Middle}</TableCell>
+                                <TableCell align="center">{user.Email}</TableCell>
                                 <TableCell align="center">
                                     <Button variant="contained" onClick={() => handleEdit(user)}>EDIT</Button>
                                 </TableCell>
@@ -243,24 +268,21 @@ function ViewUsers() {
                 <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', boxShadow: 24, p: 4 }}>
                     <Typography variant="h6" component="h2" fontWeight="bold" align="left">User Information</Typography>
                     <div style={{ marginBottom: '16px' }} />
-                    <TextField variant="outlined" 
-                    label="Email" 
-                    value={editEmail} 
-                    onChange={(e) => setEditEmail(e.target.value)} 
-                    disabled
-                    />
-                    <div style={{ marginBottom: '16px' }} />
+                    
                     <TextField variant="outlined" 
                     label="First Name" 
                     value={editFirst} 
                     onChange={(e) => setEditFirst(e.target.value)} 
-
+                    error={firstNameError}
+                    helperText={firstNameError && "First Name is required"}
                     />
                     <div style={{ marginBottom: '16px' }} />
                     <TextField variant="outlined" 
                     label="Last Name" 
                     value={editLast} 
                     onChange={(e) => setEditLast(e.target.value)} 
+                    error={lastNameError}
+                    helperText={lastNameError && "Last Name is required"}
                     />
                     <div style={{ marginBottom: '16px' }} />
                     <TextField variant="outlined" 
@@ -268,11 +290,20 @@ function ViewUsers() {
                     value={editMiddle} onChange={(e) => setEditMiddle(e.target.value)} 
                     />
                     <div style={{ marginBottom: '16px' }} />
+                    <TextField variant="outlined" 
+                    label="Email" 
+                    value={editEmail} 
+                    onChange={(e) => setEditEmail(e.target.value)} 
+                    disabled
+                    />
+                    <div style={{ marginBottom: '16px' }} />
                     <TextField type="password" 
                     variant="outlined" 
                     label="Password" 
                     value={editPassword} 
                     onChange={(e) => setEditPassword(e.target.value)} 
+                    error={passwordError}
+                    helperText={passwordError && "Password is required"}
                     />
                     <div style={{ marginBottom: '16px' }} />
                     
