dbb7307b3bba5338a003aba52f0d46f7d215381d
Kevin <jadenyuki486@gmail.com>
2024-04-16 00:11:53 +0800

    Logout fixed

--- dashboard/src/Pages/Dashboard.js
+++ dashboard/src/Pages/Dashboard.js
@@ -1,34 +1,11 @@
-import React, { useEffect } from "react";
-import { Button } from "@mui/material";
+import React from "react";
 import './Dashboard.css';
 
 function Dashboard() {
-    useEffect(() => {
-        const storedEmail = localStorage.getItem('email');
-        const isLoginPage = window.location.pathname === '/'; 
-
-       
-        if (!(storedEmail ) && !isLoginPage) {
-            window.location.href = "/";
-        }
-    }, []);
-
-    function handleLogout() {
-       
-        localStorage.removeItem('email');
-
-       
-        window.location.href = "/";
-    }
-
     return (
         <div className='main'>
             <h1>Welcome to Saint Mary's University</h1>
-            <div className="Logout">
-                <Button variant="contained" onClick={handleLogout}>
-                    Logout
-                </Button>
-            </div>
+            
         </div>
     );
 }
--- dashboard/src/Pages/Sidebar.js
+++ dashboard/src/Pages/Sidebar.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import './Sidebar.css';
 import Home from '@mui/icons-material/Home';
 import Info from '@mui/icons-material/Info';
 import Add from '@mui/icons-material/Add';
 import AccountCircle from '@mui/icons-material/AccountCircle';
+import Logout from '@mui/icons-material/Logout';
+import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
 import { Link } from "react-router-dom";
 
 function Sidebar(){
+  useEffect(() => {
+    const storedEmail = localStorage.getItem('email');
+    const isLoginPage = window.location.pathname === '/'; 
+
+   
+    if (!(storedEmail ) && !isLoginPage) {
+        window.location.href = "/";
+    }
+}, []);
+function handleLogout() {
+       
+  localStorage.removeItem('email');
+
+ 
+  window.location.href = "/";
+}
     return (
         <div className='sidebar'>
           <div className='h1'>
@@ -17,9 +35,12 @@ function Sidebar(){
           <br/>
           <Link to = "/viewstudents" style={{justifyContent: 'left', display: 'flex', color: '#FFFFFF', textDecoration: 'none'}}><Info sx={{ fontSize: 40 }}/>&nbsp;&nbsp;&nbsp;View Student</Link>
           <br/>
-          <Link to = "/viewusers" style={{justifyContent: 'left', display: 'flex', color: '#FFFFFF', textDecoration: 'none'}}><AccountCircle sx={{ fontSize: 40 }}/>&nbsp;&nbsp;&nbsp;View Users</Link>
+          <Link to = "/viewusers" style={{justifyContent: 'left', display: 'flex', color: '#FFFFFF', textDecoration: 'none'}}><VerifiedUserIcon sx={{ fontSize: 40 }}/>&nbsp;&nbsp;&nbsp;View Users</Link>
           <br/>
           <Link to = "/managestudent" style={{justifyContent: 'left', display: 'flex', color: '#FFFFFF', textDecoration: 'none'}}><AccountCircle sx={{ fontSize: 40 }}/>&nbsp;&nbsp;&nbsp;Manage Student</Link>
+          <br/>
+          <Link onClick={handleLogout} style={{justifyContent: 'left', display: 'flex', color: '#FFFFFF', textDecoration: 'none'}}><Logout sx={{ fontSize: 40 }}/>&nbsp;&nbsp;&nbsp;Logout</Link>
+          
           </div>
         </div>
     );
